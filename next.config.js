62f64a330d6e56e13b2bf242a9a48b78097546dd
Brad Bozarth <brad@classgenius.ai>
2026-03-10 20:05:43 -0400

    Redirect to new site

--- next.config.js
+++ next.config.js
@@ -4,6 +4,28 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_BASE_PATH: "/xp-ge-craft",
     },
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "https://hensin.space",
+                permanent: true,
+                basePath: false,
+            },
+            {
+                source: "/xp-ge-craft",
+                destination: "https://hensin.space",
+                permanent: true,
+                basePath: false,
+            },
+            {
+                source: "/xp-ge-craft/:path*",
+                destination: "https://hensin.space",
+                permanent: true,
+                basePath: false,
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
